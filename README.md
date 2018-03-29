a2b66f0075537cf425a674fd99253e91176d7dcf
Stephen Lam <ascendancy.sl@gmail.com>
2018-03-28 21:14:29 -0700

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# [EVA] ESLint config
+
+## Differences from Airbnb
+
+- `operator-linebreak`: `'after'`
+- `no-debugger`: `'off'` when developing
+- `no-param-reassignr`: prop reassignment allowed for `el`
+- `no-unused-expressions`: allow short circuiting (`foo && foo()`)
+
+**Also includes the Vue Eslint Plugin**
+
+## Install
+
+```bash
+$ npm install -D eslint @ascendancyy/eslint-config-eva
+```
+
+or  
+
+```bash
+$ yarn add -D eslint @ascendancyy/eslint-config-eva
+```
+
+## Usage
+
+In ESLint (.eslintrc, .eslintrc.js):
+
+```js
+{
+  "extends": ["@ascendancyy/eslint-config-eva"]
+}
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+const config = {
+  extends: [
+    'eslint-config-airbnb-base',
+    'plugin:vue/recommended',
+  ],
+  settings: {
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'operator-linebreak': ['warn', 'after', { overrides: { '=': 'none' } }],
+
+    'no-debugger': process.env.NODE_ENV !== 'development' ? 'error' : 'off',
+
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'accumulator', // for reduce accumulators
+        'e', // for e.returnvalue
+        'ctx', // for Koa routing
+        'req', // for Express requests
+        'request', // for Express requests
+        'res', // for Express responses
+        'response', // for Express responses
+        '$scope', // for Angular 1 scopes
+        'el', // for el.style
+      ],
+    }],
+    'no-unused-expressions': ['error', {
+      allowShortCircuit: true,
+      allowTernary: false,
+      allowTaggedTemplates: false,
+    }],
+
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true,
+    }],
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+      vue: 'never',
+    }],
+  },
+};
+
+try {
+  config.settings['import/resolver'] = {
+    webpack: {
+      config: require.resolve('@vue/cli-service/webpack.config.js'),
+    },
+  };
+} catch (e) {
+  // do nothing
+}
+
+module.exports = config;
